22b61dedc4a5ffd797176f636c127f9c89c04427
Silveira, Joao (EFS-GH5, extern) <silveira@itemis.de>
2022-04-11 11:10:45 +0200

    add githup packages publication to mps plugin

--- mps-plugin/build.gradle.kts
+++ mps-plugin/build.gradle.kts
@@ -73,5 +73,13 @@ publishing {
     }
     repositories {
         mavenLocal()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/modelix/mps-rest-model-access")
+            credentials {
+                username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as? String ?: System.getenv("TOKEN")
+            }
+        }
     }
 }
