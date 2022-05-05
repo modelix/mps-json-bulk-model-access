fe602856bc663138480c30eaf82368ac4436804a
Kolja Dummann <kolja.dummann@itemis.de>
2022-05-05 17:04:09 +0200

    use kotlin 1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.6.10" apply false
-    kotlin("plugin.serialization") version "1.6.10" apply false
+    kotlin("multiplatform") version "1.6.20" apply false
+    kotlin("plugin.serialization") version "1.6.20" apply false
 }
 
 group = "org.modelix.mps-rest-model-access"
