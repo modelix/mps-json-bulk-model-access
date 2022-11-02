1d61e2b92f3056a1b186628fc925f961c5ed7698
Norman Köster <nkoester@itemis.com>
2022-11-02 16:33:54 +0100

    Add nexus publication for mps-plugin component

--- mps-plugin/build.gradle.kts
+++ mps-plugin/build.gradle.kts
@@ -81,17 +81,15 @@ publishing {
                 password = project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
             }
         }
-        if (project.hasProperty("artifacts.itemis.cloud.user")) {
-            maven {
-                name = "itemis"
-                url = if (version.toString().contains("SNAPSHOT"))
-                    uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
-                else
-                    uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
-                credentials {
-                    username = project.findProperty("artifacts.itemis.cloud.user").toString()
-                    password = project.findProperty("artifacts.itemis.cloud.pw").toString()
-                }
+        maven {
+            name = "itemis"
+            url = if (version.toString().contains("SNAPSHOT"))
+                uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
+            else
+                uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
+            credentials {
+                username = project.findProperty("artifacts.itemis.cloud.user").toString()
+                password = project.findProperty("artifacts.itemis.cloud.pw").toString()
             }
         }
     }
