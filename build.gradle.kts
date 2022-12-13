65a170f50661eca5e25e9eec1b531869af155ae4
Norman Köster <nkoester@itemis.com>
2022-12-13 17:38:16 +0100

    Bump to modelix 1.4.10 + bump dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "1.7.10" apply false
-    kotlin("plugin.serialization") version "1.7.10" apply false
-     id("com.palantir.git-version") version "0.13.0"
+    kotlin("multiplatform") version "1.7.22" apply false
+    kotlin("plugin.serialization") version "1.7.22" apply false
+    id("com.palantir.git-version") version "0.15.0"
 }
 
 group = "org.modelix.mps-json-bulk-model-access"
